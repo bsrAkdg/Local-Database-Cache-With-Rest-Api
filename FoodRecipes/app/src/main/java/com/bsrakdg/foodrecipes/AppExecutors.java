24746cb497093ca428cc9bbdefb44ac24d801ec1
bsrAkdg <busraaakdag@gmail.com>
2020-04-23 23:03:08 +0300

    Executors

--- FoodRecipes/app/src/main/java/com/bsrakdg/foodrecipes/AppExecutors.java
+++ FoodRecipes/app/src/main/java/com/bsrakdg/foodrecipes/AppExecutors.java
@@ -1,23 +1,42 @@
 package com.bsrakdg.foodrecipes;
 
 
+import android.os.Handler;
+import android.os.Looper;
+
+import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 
 public class AppExecutors {
 
     private static AppExecutors instance;
 
-    public static AppExecutors getInstance(){
-        if(instance == null){
+    private final Executor mDiskIO = Executors.newSingleThreadExecutor();
+    private final Executor mMainThreadExecutor = new MainThreadExecutor();
+
+    public static AppExecutors getInstance() {
+        if (instance == null) {
             instance = new AppExecutors();
         }
         return instance;
     }
 
-    private final ScheduledExecutorService mNetworkIO = Executors.newScheduledThreadPool(3);
-
-    public ScheduledExecutorService networkIO(){
-        return mNetworkIO;
+    public Executor getDiskIO() {
+        return mDiskIO;
     }
+
+    public Executor getMainThreadExecutor() {
+        return mMainThreadExecutor;
+    }
+
+    private static class MainThreadExecutor implements Executor {
+
+        private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+        @Override
+        public void execute(Runnable command) {
+            mainThreadHandler.post(command);
+        }
+    }
+
 }
